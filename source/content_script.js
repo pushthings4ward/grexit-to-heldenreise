7bac39f56da688fa1cddcfa7ca0a41b7c995ef9e
cosmin <cosmin.cab@gmail.com>
2015-06-29 21:01:11 +0200

    semantic bug fixing

--- source/content_script.js
+++ source/content_script.js
@@ -1,19 +1,21 @@
 walk(document.body);
 
-function walk(node) 
+document.title = replaceText(document.title);
+
+function walk(node)
 {
-	// I stole this function from here:
+	// I copied this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
 		case 1:  // Element
 		case 9:  // Document
 		case 11: // Document fragment
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
 				next = child.nextSibling;
 				walk(child);
@@ -27,24 +29,45 @@ function walk(node)
 	}
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
-
-	v = v.replace(/\bden Grexit\b/g, "die Heldenreise");
-	v = v.replace(/\bder Grexit\b/g, "die Heldenreise");
-	v = v.replace(/\bGREXIT\b/g, "die Heldenreise");
-	v = v.replace(/\bGrexit,\b/g, "die Heldenreise,");
-	v = v.replace(/\bGrexit\b/g, "Heldenreise");
-	v = v.replace(/\bein Grexit,\b/g, "eine Heldenreise");
-	v = v.replace(/\bEin Grexit,\b/g, "Eine Heldenreise");
-	v = v.replace(/\b"Grexits"\b/g, "'Heldenreise'");
-	v = v.replace(/\beinen Grexit,\b/g, "eine Heldenreise,");
-	v = v.replace(/\bEinen Grexit,\b/g, "Eine Heldenreise,");
-	v = v.replace(/"/g, ""); //remove all quotes
-	
-	
-	textNode.nodeValue = v;
+function handleText(textNode) {
+  textNode.nodeValue = replaceText(textNode.nodeValue);
 }
 
+function replaceText(v)
+{
+
+
+	// Remove and Replace
+	
+	
+	//v = v.replace(/\bGrexit\b/g, "die Heldenreise");
+	
+	
+	v = v.replace(/\bist der GREXIT\b/ig, " ist die Heldenreise ");
+	v = v.replace(/\bder GREXIT\b/ig, " die Heldenreise ");
+	v = v.replace(/\sden Grexit\s/ig, " die Heldenreise ");
+	v = v.replace(/\sein Grexit\s/g, " eine Heldenreise ");
+	v = v.replace(/\sEin Grexit\s/g, " Eine Heldenreise ");
+	v = v.replace(/\szum Grexit\s/ig, " zur Heldenreise ");
+	v = v.replace(/\sbeim Grexit\s/ig, " bei der Heldenreise ");
+	v = v.replace(/\sBEIM GREXIT\s/ig, " BEI DER HELDENREISE ");
+	v = v.replace(/\sdem Grexit\s/ig, " der Heldenreise ");
+	v = v.replace(/\svor dem Grexit\s/ig, " der Heldenreise ");
+	v = v.replace(/\sDer drohende Grexit\s/g, " Die drohende Heldenreise ");
+	v = v.replace(/\svor dem Grexit\s/ig, " der Heldenreise ");
+	v = v.replace(/\sbeim "Grexit"\s/ig, " bei der 'Heldenreise' ");
+	v = v.replace(/\s"Grexits"\s/ig, " 'Heldenreise' ");
+	v = v.replace(/\sdes Grexits\s/ig, " der Heldenreise ");
+	v = v.replace(/\sEin Grexit,\s/ig, " Eine Heldenreise, ");
+	v = v.replace(/\sEinen Grexit,\s/ig, " Eine Heldenreise, ");
+	v = v.replace(/\seinen Grexit,\s/ig, " eine Heldenreise, ");
+	v = v.replace(/\sein Grexit,\s/ig, " eine Heldenreise, ");
+	v = v.replace(/\seinem Grexit\s/ig, " einer Heldenreise ");
+	v = v.replace(/\seines Grexit\s/ig, " einer Heldenreise ");
+	v = v.replace(/\bGrexit\b/ig, "Heldenreise");
+
+	
+
+	return v;
+}
 
